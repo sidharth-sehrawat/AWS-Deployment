e6c56760a9c742a53eaa08d44bdb0670c5c4da64
Sidharth Sehrawat <sid2001sehrawat@gmail.com>
2026-06-06 22:20:33 +0530

    Test change in vpc.tf , removed egress

--- terraform/environments/dev/vpc.tf
+++ terraform/environments/dev/vpc.tf
@@ -145,12 +145,6 @@ resource "aws_security_group" "mario" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "${var.project_name}-mario-sg"
